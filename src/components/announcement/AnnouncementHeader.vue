f5eab0bc835d2b17220b3e1faf8220bb6ff6bc8e
seungmin-park <tmddudals369@naver.com>
2022-04-19 20:57:27 +0900

    검색 조건 추가 시작

--- src/components/announcement/AnnouncementHeader.vue
+++ src/components/announcement/AnnouncementHeader.vue
@@ -1,8 +1,25 @@
 <template>
   <div>
     <div class="search-condition">
-      <button v-on:click="emp">채용</button>
-      <button v-on:click="edu">교육</button>
+      <span>
+        <button v-on:click="emp">채용</button>
+        <button v-on:click="edu">교육</button>
+      </span>
+      <button @click="changePositionStatus">직무</button>
+      <div class="search-condition-position" v-if="positionStatus">
+        <ul>
+          <li v-for="position in positions" :key="position.id">
+            <label :for="position">
+              <input
+                type="checkbox"
+                :name="positions"
+                :id="position"
+                :value="position"
+              />{{ position }}
+            </label>
+          </li>
+        </ul>
+      </div>
     </div>
     <div>
       <router-link :to="{ path: '/addAnnounce' }">글쓰기</router-link>
@@ -11,16 +28,33 @@
 </template>
 
 <script>
+import positions from "../../data/positon";
 export default {
   data() {
-    return {};
+    return {
+      positionStatus: false,
+      type: null,
+      positions: positions,
+    };
   },
   methods: {
+    changePositionStatus() {
+      if (this.positionStatus) {
+        this.positionStatus = false;
+      } else {
+        this.positionStatus = true;
+      }
+    },
     emp() {
-      this.emitter.emit("emp", "emp");
+      this.type = "emp";
+      this.submitType();
     },
     edu() {
-      this.emitter.emit("edu", "edu");
+      this.type = "edu";
+      this.submitType();
+    },
+    submitType() {
+      this.emitter.emit("type", this.type);
     },
   },
 };
@@ -29,5 +63,17 @@ export default {
 <style scoped>
 .search-condition {
   padding: 25px;
+}
+
+.search-condition-position {
+  width: 385px;
+  height: 300px;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+}
+
+ul {
+  list-style: none;
+  padding-left: 0px;
 }
 </style>
