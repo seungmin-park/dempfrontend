c8f0557342ba6bfcb20436ce0300d456e5e3ce81
seungmin-park <tmddudals369@naver.com>
2022-05-30 16:38:15 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ const target = "https://dempbackend.herokuapp.com/";
 
 module.exports = {
   devServer: {
-    port: 15986,
+    port: 10233,
     proxy: {
       //proxy 요청을 보낼 api 시작 부분
       "^/api": {
