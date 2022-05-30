1a83507e5ad9b456386719244c987481b25ae3d2
seungmin-park <tmddudals369@naver.com>
2022-05-31 01:37:27 +0900

    change server config

--- vue.config.js
+++ vue.config.js
@@ -3,14 +3,13 @@ module.exports = defineConfig({
   transpileDependencies: true,
 });
 
-const target = "https://dempbackend.herokuapp.com/";
+const target = "https://dempbackend.herokuapp.com";
 
 module.exports = {
   devServer: {
-    port: 50339,
     proxy: {
       //proxy 요청을 보낼 api 시작 부분
-      "^/api": {
+      "/api": {
         target,
         changeOrigin: true,
       },
